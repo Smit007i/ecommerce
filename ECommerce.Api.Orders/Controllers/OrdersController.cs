d97f83ac8a617f6952bd1bcd0e69cbdc61b0f430
Smit007i <nechitaylo.ser@gmail.com>
2022-06-11 22:23:45 +0300

    Microservices
    Azure Microservices with .NET Core for Developers

---
+++ ECommerce.Api.Orders/Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using ECommerce.Api.Orders.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Orders.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrdersProvider ordersProvider;
+
+        public OrdersController(IOrdersProvider ordersProvider)
+        {
+            this.ordersProvider = ordersProvider;
+        }
+
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> GetOrderAsync(int id)
+        //{
+        //    var result = await ordersProvider.GetOrderAsync(id);
+        //    if (result.IsSuccess)
+        //    {
+        //        return Ok(result.Order);
+        //    }
+        //    return NotFound();
+        //}
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetOrdersAsync(int customerId)
+        {
+            var result = await ordersProvider.GetOrdersAsync(customerId);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Orders);
+            }
+            return NotFound();
+        }
+    }
+}
